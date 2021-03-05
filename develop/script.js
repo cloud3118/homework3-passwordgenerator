1dc4da5ea697e5b1e0839b9c6c35bee32bdc28ea
Casey Loudermilk <cloud3118@gmail.com>
2021-03-04 21:51:43 -0500

    finished conditionals for prompts

--- develop/script.js
+++ develop/script.js
@@ -26,19 +26,32 @@ function generatePassword() {
   var charLength = parseInt(prompt("How Many Characters Do You Want Your Password to Be?\n(Between 8-128)"));
    if (charLength >=8 && charLength <=128){
    passLength = charLength;
-  }
+   console.log(charLength);
+   }
   else {
     alert("Try Again");
     return;
   }
-
-
-
+  
   var promptNumber = confirm("Include Numbers?");
-  var promptUpperCase = confirm("Include Upper Case Letters?");
-  var promptLowerCase = confirm("Include Lower Case Letters?");
-  var promptSymbol = confirm("Include Symbols?");
+  if (promptNumber === true) {
+    console.log("including numbers");
+  }
 
+  var promptUpperCase = confirm("Include Upper Case Letters?");
+  if (promptUpperCase) {
+    console.log("including Upper Case");
+  }
+  
+  var promptLowerCase = confirm("Include Lower Case Letters?");
+  if (promptLowerCase) {
+    console.log("including lower case");
+  }
+  
+  var promptSymbol = confirm("Include Symbols?");
+  if (promptSymbol) {
+    console.log("including symbols");
+  }
 
 
   return finalPassword;
